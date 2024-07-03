e7f4eb1ee584e62448530825e0b83b7ecb508cb0
kunihiro-dev <49485046+kunihiro-dev@users.noreply.github.com>
2024-07-03 10:20:20 +0000

    first commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,34 @@
+import type { MetaFunction } from "@remix-run/node";
+import { ModeToggle } from "~/components/mode-toggle";
+import { Button } from "~/components/ui/button";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="font-sans p-4">
+      <ModeToggle />
+      <h1 className="text-3xl">Welcome to Remix</h1>
+      <ul className="list-disc mt-4 pl-6 space-y-2">
+        <li>
+          <a
+            className="text-blue-700 underline visited:text-purple-900"
+            target="_blank"
+            href="https://remix.run/start/quickstart"
+            rel="noreferrer"
+          >
+            5m Quick Start
+          </a>
+        </li>
+        <li>
+          <Button>Click me</Button>
+        </li>
+      </ul>
+    </div>
+  );
+}
